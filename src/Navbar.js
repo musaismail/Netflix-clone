b6da70a0e71a11e02cbea84ba1bc57125f2f721f
Ismail Musa <imusa1438@gmail.com>
2022-08-17 19:04:29 -0700

    first commit

---
+++ src/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+  const [show, handleShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        handleShow(true);
+      } else handleShow(false);
+    });
+    return () => {
+      window.removeEventListener("scroll", handleShow);
+    };
+  }, []);
+  return (
+    <div className={`nav ${show && "nav_black"} `}>
+      <img
+        className="nav_logo"
+        src="https://imgix.bustle.com/uploads/image/2017/8/29/c8c8077a-10fc-44d5-93f0-da4e592a299e-netflix-logo-print_pms.jpg?w=800&fit=crop&crop=faces&auto=format%2Ccompress"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+        alt="avatar"
+      />
+    </div>
+  );
+}
+
+export default Navbar;
